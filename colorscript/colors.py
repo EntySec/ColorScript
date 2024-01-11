d9b7c7374f9d79b5701e0658abf6ab9d84d237aa
Ivan Nikolskiy <enty8080@gmail.com>
2024-01-11 02:15:13 +0100

    Update colors.py

--- colorscript/colors.py
+++ colorscript/colors.py
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import sys
-
 
 class Colors(object):
     """ Subclass of colorscript module.
@@ -35,23 +33,23 @@ class Colors(object):
     def __init__(self) -> None:
         super().__init__()
 
-        self.BLACK = '\033[30m' if sys.platform == 'darwin' else '\001\033[30m\002'
-        self.RED = '\033[31m' if sys.platform == 'darwin' else '\001\033[31m\002'
-        self.GREEN = '\033[32m' if sys.platform == 'darwin' else '\001\033[32m\002'
-        self.YELLOW = '\033[33m' if sys.platform == 'darwin' else '\001\033[33m\002'
-        self.BLUE = '\033[34m' if sys.platform == 'darwin' else '\001\033[34m\002'
-        self.PURPLE = '\033[35m' if sys.platform == 'darwin' else '\001\033[35m\002'
-        self.CYAN = '\033[36m' if sys.platform == 'darwin' else '\001\033[36m\002'
-        self.WHITE = '\033[77m' if sys.platform == 'darwin' else '\001\033[37m\002'
+        self.BLACK = '\033[30m'
+        self.RED = '\033[31m'
+        self.GREEN = '\033[32m'
+        self.YELLOW = '\033[33m'
+        self.BLUE = '\033[34m'
+        self.PURPLE = '\033[35m'
+        self.CYAN = '\033[36m'
+        self.WHITE = '\033[77m'
 
-        self.END = '\033[0m' if sys.platform == 'darwin' else '\001\033[0m\002'
-        self.BOLD = '\033[1m' if sys.platform == 'darwin' else '\001\033[1m\002'
-        self.DARK = '\033[2m' if sys.platform == 'darwin' else '\001\033[2m\002'
-        self.BENT = '\033[3m' if sys.platform == 'darwin' else '\001\033[3m\002'
-        self.LINE = '\033[4m' if sys.platform == 'darwin' else '\001\033[4m\002'
-        self.TWINK = '\033[5m' if sys.platform == 'darwin' else '\001\033[5m\002'
-        self.BACK = '\033[7m' if sys.platform == 'darwin' else '\001\033[7m\002'
+        self.END = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.DARK = '\033[2m'
+        self.BENT = '\033[3m'
+        self.LINE = '\033[4m'
+        self.TWINK = '\033[5m'
+        self.BACK = '\033[7m'
 
-        self.REMOVE = '\033[1K\r' if sys.platform == 'darwin' else '\001\033[1K\r\002'
-        self.CLEAR = '\033[H\033[J' if sys.platform == 'darwin' else '\001\033[H\033[J\002'
-        self.NEWLINE = '\n' if sys.platform == 'darwin' else '\001\n\002'
+        self.REMOVE = '\033[1K\r'
+        self.CLEAR = '\033[H\033[J'
+        self.NEWLINE = '\n'
