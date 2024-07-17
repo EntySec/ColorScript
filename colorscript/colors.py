f4dd1e9397f8affbc85fb8acfcf5a2d33cbfae48
Ivan Nikolskiy <enty8080@gmail.com>
2024-07-17 22:47:33 +0200

    Update colors.py

--- colorscript/colors.py
+++ colorscript/colors.py
@@ -23,33 +23,23 @@ SOFTWARE.
 """
 
 
-class Colors(object):
-    """ Subclass of colorscript module.
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+PURPLE = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[77m'
 
-    This subclass of colorscript module is intended for
-    providing different color bindings for interface.
-    """
+END = '\033[0m'
+BOLD = '\033[1m'
+DARK = '\033[2m'
+BENT = '\033[3m'
+LINE = '\033[4m'
+TWINK = '\033[5m'
+BACK = '\033[7m'
 
-    def __init__(self) -> None:
-        super().__init__()
-
-        self.BLACK = '\033[30m'
-        self.RED = '\033[31m'
-        self.GREEN = '\033[32m'
-        self.YELLOW = '\033[33m'
-        self.BLUE = '\033[34m'
-        self.PURPLE = '\033[35m'
-        self.CYAN = '\033[36m'
-        self.WHITE = '\033[77m'
-
-        self.END = '\033[0m'
-        self.BOLD = '\033[1m'
-        self.DARK = '\033[2m'
-        self.BENT = '\033[3m'
-        self.LINE = '\033[4m'
-        self.TWINK = '\033[5m'
-        self.BACK = '\033[7m'
-
-        self.REMOVE = '\033[1K\r'
-        self.CLEAR = '\033[H\033[J'
-        self.NEWLINE = '\n'
+REMOVE = '\033[1K\r'
+CLEAR = '\033[H\033[J'
+NEWLINE = '\n'
